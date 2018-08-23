7e756de162764eb31ad7d9da40f069654382392d
taylornok <taylorowenskees@outlook.com>
2018-08-22 18:17:52 -0700

    update seeds

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Bacon Cheeseburger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Baja Burger", 0);
