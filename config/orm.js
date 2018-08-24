7eeabae65a3b7f434c995929a41342c3cb892de3
taylornok <taylorowenskees@outlook.com>
2018-08-23 22:30:26 -0700

    config, started models

--- config/orm.js
+++ config/orm.js
@@ -4,70 +4,54 @@ var connection = require("../config/connection.js");
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// The above helper function loops through and insertOnes an array of question marks - ["?", "?", "?"] - and turns it into a string.
 // ["?", "?", "?"].toString() => "?,?,?";
-// function selectAll(num) {
-//   var arr = [];
-
-//   for (var i = 0; i < num; i++) {
-//     arr.push("?");
-//   }
-
-//   return arr.toString();
-// }
-
-// // Helper function to convert object key/value pairs to SQL syntax
-// function objToSql(ob) {
-//   var arr = [];
-
-//   // loop through the keys and push the key/value as a string int arr
-//   for (var key in ob) {
-//     var value = ob[key];
-//     // check to skip hidden properties
-//     if (Object.hasOwnProperty.call(ob, key)) {
-//       // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-//       if (typeof value === "string" && value.indexOf(" ") >= 0) {
-//         value = "'" + value + "'";
-//       }
-//       // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-//       // e.g. {sleepy: true} => ["sleepy=true"]
-//       arr.push(key + "=" + value);
-//     }
-//   }
-
-//   // translate array of strings to a single comma-separated string
-//   return arr.toString();
-// }
-
-// Define methods
-
-function selectAll(burgers){
-    var array = [];
-    for (let i = 0; i < burgers;  i++) {
-        array.push('?');           
-    }
-    return array.toString();
+function printQuestionmarks(num){
+  var array = [];
+  for (var i=0; i < num; i++){
+    array.push('?')
+  };
+  return array.toString();
 };
+// Helper function to convert object key/value pairs to SQL syntax
+function convObj(obj){
+  var array = [];
 
-function insertOne(burger){
-
-};
-function updateOne(burger){
-
-};
-
-// Object for all our SQL statement functions.
-var orm = {
-  all: function(tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
+   // loop through the keys and push the key/value as a string int arr
+  for (var key in obj) {
+    var value = obj[key];
+    //check to skip hidden properties
+    if(Object.hasOwnProperty.cselectAll(obj, key)){
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
       }
-      cb(result);
-    });
-  },
-  create: function(table, cols, vals, cb) {
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);    }
+  }
+
+  return array.toString();
+};
+
+
+// Object for selectAll our SQL statement functions.
+var orm = {
+
+
+    selectAll: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+  
+
+
+  insertOne: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -88,11 +72,11 @@ var orm = {
     });
   },
   // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "updateOne " + table;
 
     queryString += " SET ";
-    queryString += objToSql(objColVals);
+    queryString += convObj(objColVals);
     queryString += " WHERE ";
     queryString += condition;
 
